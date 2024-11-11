9408abea6495b9a574dec4e96058a39a9e81f769
shinyeji-spea <shin020516@gmail.com>
2024-11-11 12:23:18 +0900

    Fix: dev 내 오류 코드 수정 및 코드 통일

--- NowPrompt/function.py
+++ NowPrompt/function.py
@@ -18,7 +18,7 @@ prompt_files = {
     "recom": os.path.join(engineering_dir, 'recomm.txt'),
     "reportVB" : os.path.join(variable_dir, 'report.txt'),
     "recomVB" : os.path.join(variable_dir, 'recom.txt'),
-    "dbVB" : os.path.join(variable_dir, 'MDB.txt'),
+    "dbVB" : os.path.join(variable_dir, 'MDB.txt')
 }
 
 # 추천 질문 생성
